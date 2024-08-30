835237c019c52063ce61a7b5ba1909fa2a884f79
deborafarias <deborafariasp@gmail.com>
2024-08-29 22:41:37 -0300

    initial-commit

--- eventos.js
+++ eventos.js
@@ -4,6 +4,8 @@ div.addEventListener('click', function() {
     alert('Hola! Soy el Div');
 })
  
+//Solución que ya había implementado
+
 const button = document.getElementById('button');
 
 button.addEventListener('click', (event)=>{
